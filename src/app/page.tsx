0c005637d49d3e9d633e3cd70bd6860e1c5c483b
Daniel Sims <daniel.sims.02@gmail.com>
2024-03-30 00:23:49 +1000

    Wrap attribution elements on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,15 +21,15 @@ export default function Home() {
         <p className="text-white font-light text-sm mb-2 tracking-wide">Chat Input</p>
         <p className="text-[#888] font-light text-sm tracking-wide">A chat input for interacting with language models. Subtle animations indicate the current state of the model.</p>
         <hr className="my-16 border-[#333] border-opacity-50" />
-        <div className="flex flex-row flex-wrap justify-between text-white">
+        <div className="flex flex-row flex-wrap gap-16 justify-between text-white">
 
-          <StateToggle
-            states={states as readonly State[]}
-            activeState={controlledState ?? 'idle'}
-            onStateChange={setControlledState}
-          />
+            <StateToggle
+              states={states as readonly State[]}
+              activeState={controlledState ?? 'idle'}
+              onStateChange={setControlledState}
+            />
 
-          <div className="flex justify-end items-center gap-4 tracking-wide">
+          <div className="flex flex-row-reverse md:flex-row justify-end items-center gap-4 tracking-wide w-full md:w-fit">
             <span className='text-xs font-light'>Built by danielsims</span>
             <Link href={'https://github.com/danielsims/chat-ui'}><FaGithub className="text-white fill-white" size={24} /></Link>
           </div>
