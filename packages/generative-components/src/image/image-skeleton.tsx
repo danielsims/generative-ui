2269c0815e8646eaa302214026a8830ea3dd0aa5
Daniel Sims <daniel.sims.02@gmail.com>
2024-04-08 20:23:56 +1000

    Add captions to image component

--- packages/generative-components/src/image/image-skeleton.tsx
+++ packages/generative-components/src/image/image-skeleton.tsx
@@ -1,7 +1,7 @@
 export function GenerativeImageSkeleton({ prompt }: { prompt: string }) {
   return (
     <div className="shadow-inner-dark flex aspect-square h-auto w-full items-center justify-center rounded-xl border border-[#252525]">
-      <p className="w-full text-center">{prompt}</p>
+      <p className="w-full p-8 text-center">{prompt}</p>
     </div>
   );
 }
