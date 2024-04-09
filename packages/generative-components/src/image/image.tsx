aa8cf5308d8615be4e7a970deb8b8218798aed40
Daniel Sims <daniel.sims.02@gmail.com>
2024-04-09 23:52:06 +1000

    Update image component

--- packages/generative-components/src/image/image.tsx
+++ packages/generative-components/src/image/image.tsx
@@ -1,7 +1,11 @@
 export function GenerativeImage({ src, alt }: { src: string; alt: string }) {
   return (
     <div className="shadow-inner-dark group relative aspect-square h-auto w-full rounded-xl border border-[#252525]">
-      <img src={src} alt={alt} className="h-auto max-w-full rounded-xl" />
+      <img
+        src={src}
+        alt={alt}
+        className="h-auto w-full max-w-full rounded-xl"
+      />
       <div className="absolute inset-0 hidden items-center gap-4 p-8 transition-all group-hover:flex group-hover:bg-black/20">
         <div className="absolute bottom-12 left-0 flex max-h-[300px] w-full flex-col gap-6 px-12">
           <div className="font-mono">Dall-E 3</div>
